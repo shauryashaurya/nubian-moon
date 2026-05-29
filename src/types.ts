00b17309969c323694701a2c0e50629eecac50e0
Shaurya Agarwal <shauryashaurya@gmail.com>
2026-05-29 21:14:30 +0530

    minor fixes

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,4 @@
-import type { CSSProperties } from 'react';
+// import type { CSSProperties } from 'react';
 
 export type LayoutDirection = 'horizontal-ltr' | 'horizontal-rtl' | 'vertical-rl' | 'vertical-lr';
 
